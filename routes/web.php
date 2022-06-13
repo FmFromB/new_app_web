4883f60051230ef79964dc403de3bc250f599a3e
isotrop <linkinkingsman@gmail.com>
2022-06-13 23:21:48 +1000

    done3

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/projects', function () {
+    return view('projects');
+})->name('projects');
+
+Route::get('/news', function () {
+    return view('news');
+})->name('news');
+
+Route::get('/contacts', function () {
+    return view('contacts');
+})->name('contacts');
